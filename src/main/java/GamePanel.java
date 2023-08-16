0cd95196c8ebe14c7080b593d25f1f6fc1565fc7
kilian <kiliantennyson@live.ie>
2023-08-16 13:36:23 +0100

    Added Keyhandler

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -4,18 +4,18 @@ import java.awt.*;
 public class GamePanel extends JPanel implements Runnable {
 
     // Screen Settings
-    final int originalTileSize = 16;
-    final int scale = 3;
-    final int tileSize = originalTileSize * scale;
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol;    // 768 Pixels
-    final int screenHeight = tileSize * maxScreenRow;   // 576 Pixels
+    final int ORIGINAL_TILE_SIZE = 16;
+    final int SCALE = 3;
+    final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE;
+    final int MAX_SCREEN_COL = 16;
+    final int MAX_SCREEN_ROW = 12;
+    final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COL;    // 768 Pixels
+    final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW;   // 576 Pixels
 
     Thread gameThread;
 
     public GamePanel() {
-        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
     }
@@ -25,9 +25,26 @@ public class GamePanel extends JPanel implements Runnable {
         gameThread.start();
     }
 
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+        g2.fillRect(10, 10, TILE_SIZE, TILE_SIZE);
+        g2.dispose();
+    }
+
     @Override
     public void run() {
-
+        while(gameThread != null){
+            update();
+            repaint();
+        }
     }
 
 }
