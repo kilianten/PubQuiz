45f8e394d1ed1c29924c16e08e38d054f3d92812
kilian <kiliantennyson@live.ie>
2023-08-16 13:08:28 +0100

    Initial commit. JFrame setup and Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Pub Quiz");
+
+        window.setLocation(null);
+        window.setVisible(true);
+
+    }
+
+}
